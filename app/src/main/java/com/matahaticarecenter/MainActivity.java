729195c41ad092eeb1f4c78b8b6bcc6b2ddf4e63
fadhlis <39572912+fadhlis@users.noreply.github.com>
2019-10-18 00:13:37 +0700

    slider & main grid layout

--- app/src/main/java/com/matahaticarecenter/MainActivity.java
+++ app/src/main/java/com/matahaticarecenter/MainActivity.java
@@ -2,12 +2,18 @@ package com.matahaticarecenter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+
+import com.matahaticarecenter.adapter.SliderAdapter;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +40,20 @@ public class MainActivity extends AppCompatActivity {
         buttonOnclick(contactBtn, ContactActivity.class, "");
         buttonOnclick(program2Btn, ProgramActivity.class, "PROGRAM2");
         buttonOnclick(partnerBtn, PartnerActivity.class, "");
+
+        SliderView sliderView = findViewById(R.id.main_image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(context);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+        sliderView.startAutoCycle();
     }
 
     private void buttonOnclick(CardView cardView, final Class c, final String somekey) {
