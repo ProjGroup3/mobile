c68c97c6ca9be1870167ed8c35d665063c3d4538
MohAmin <47808616+MohAminFathullah@users.noreply.github.com>
2019-11-27 13:29:37 +0700

    update login & register

---
+++ app/src/main/java/com/matahaticarecenter/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.matahaticarecenter;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnlogin;
+    TextView singup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnlogin = findViewById(R.id.btnlogin);
+        singup = findViewById(R.id.singup);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        singup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
