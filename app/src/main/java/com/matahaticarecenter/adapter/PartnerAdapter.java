d5734389e53b9acde47cbe98f979eac84d9b1a15
MohAmin <47808616+MohAminFathullah@users.noreply.github.com>
2019-12-12 16:06:19 +0700

    updatepartner

--- app/src/main/java/com/matahaticarecenter/adapter/PartnerAdapter.java
+++ app/src/main/java/com/matahaticarecenter/adapter/PartnerAdapter.java
@@ -17,49 +17,8 @@ import com.matahaticarecenter.model.PartnerModel;
 
 import java.util.List;
 
-public class PartnerAdapter extends RecyclerView.Adapter<PartnerAdapter.ViewHolder> {
+public class PartnerAdapter  {
 
-    private List<PartnerModel> partnerModels;
-    private Context context;
-
-    public PartnerAdapter(List<PartnerModel> partner, Context context) {
-        this.partnerModels = partner;
-        this.context = context;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_partner, parent, false));
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
-        Glide.with(context).load(partnerModels.get(position).getLogo()).into(holder.imageView);
-
-        String text = partnerModels.get(position).getName();
-        if (text.length() > 60) {
-            text = text.substring(0, 60) + " ...";
-        }
-        holder.textView.setText(text);
-    }
-
-    @Override
-    public int getItemCount() {
-        return partnerModels.size();
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-        private RelativeLayout cardView;
-        private ImageView imageView;
-        private TextView textView;
-
-        ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            cardView = itemView.findViewById(R.id.item_partner_card);
-            imageView = itemView.findViewById(R.id.item_partner_logo);
-            textView = itemView.findViewById(R.id.item_partner_name);
-        }
-    }
+    
 }
 
